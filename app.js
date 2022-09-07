774b33408d32417d833d9d1673b67cb46a4415a4
samuel129 <41707531+samuel129@users.noreply.github.com>
2022-09-07 01:55:53 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 'use strict';
 
-const switcher = document.querySelector('.btn');
+const lightSwitch = document.querySelector('.light-switch');
 
-switcher.addEventListener('click', function() {
+lightSwitch.addEventListener('click', function() {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
 
 	const className = document.body.className;
 	if (className == "light-theme"){
-		this.textContent = "dark";
+		this.textContent = "Dark";
 	} else {
-		this.textContent = "light";	
+		this.textContent = "Light";	
 	}
 
 	console.log("current class name: ", className)
+});
+
+const incrementBtn = document.querySelector('.incrementBtn');
+const countHeader = document.getElementById("count");
+const decrementBtn = document.querySelector('.decrementBtn');
+
+countHeader.textContent = localStorage.getItem("Increment");
+
+incrementBtn.addEventListener('click', function() {
+	countHeader.textContent = (+countHeader.textContent + 1);
+	localStorage.setItem("Increment", countHeader.textContent);
+});
+
+decrementBtn.addEventListener('click', function(){
+	countHeader.textContent = (+countHeader.textContent - 1);
+	localStorage.setItem("Increment", countHeader.textContent);
 });
