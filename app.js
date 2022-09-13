50fa0b2c8654e003549e36b1bb9dd94af55925a7
samuel129 <41707531+samuel129@users.noreply.github.com>
2022-09-13 11:41:33 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 
 const lightSwitch = document.querySelector('.light-switch');
 
+let lightStatus = localStorage.getItem("lightStatus");
+
+if (lightStatus) {
+	document.body.className = lightStatus;
+}
+
 lightSwitch.addEventListener('click', function() {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
@@ -9,25 +15,88 @@ lightSwitch.addEventListener('click', function() {
 	const className = document.body.className;
 	if (className == "light-theme"){
 		this.textContent = "Dark";
+		console.log(this);
 	} else {
 		this.textContent = "Light";	
 	}
 
-	console.log("current class name: ", className)
+	localStorage.setItem("lightStatus", className);
 });
 
-const incrementBtn = document.querySelector('.incrementBtn');
-const countHeader = document.getElementById("count");
-const decrementBtn = document.querySelector('.decrementBtn');
 
-countHeader.textContent = localStorage.getItem("Increment");
 
-incrementBtn.addEventListener('click', function() {
-	countHeader.textContent = (+countHeader.textContent + 1);
-	localStorage.setItem("Increment", countHeader.textContent);
-});
 
-decrementBtn.addEventListener('click', function(){
-	countHeader.textContent = (+countHeader.textContent - 1);
-	localStorage.setItem("Increment", countHeader.textContent);
-});
+if (document.getElementById("count")) {
+	const countHeader = document.getElementById("count");
+	const incrementBtn = document.querySelector('.incrementBtn');
+	const decrementBtn = document.querySelector('.decrementBtn');
+	const resetBtn = document.querySelector('.resetBtn');
+	countHeader.textContent = localStorage.getItem("Increment");
+
+	incrementBtn.addEventListener('click', function() {
+		countHeader.textContent = (+countHeader.textContent + 1);
+		localStorage.setItem("Increment", countHeader.textContent);
+	});
+
+	decrementBtn.addEventListener('click', function() {
+		countHeader.textContent = (+countHeader.textContent - 1);
+		localStorage.setItem("Increment", countHeader.textContent);
+	});
+
+	resetBtn.addEventListener('click', function(){
+		if (countHeader.textContent == '0') {
+			return;
+		}
+
+
+		if (confirm("Are you sure you want to reset the counter?")) {
+			countHeader.textContent = '0';
+		}
+		localStorage.setItem("Increment", countHeader.textContent);
+	});
+}
+
+
+
+if (document.getElementById("todolist")) {
+	const inputBox = document.getElementById('inputText');
+	const submitBtn = document.querySelector('.submitBtn');
+	const ul = document.getElementById('ul');
+
+	inputBox.addEventListener("keypress", function(){
+		if (event.key === 'Enter'){
+			event.preventDefault();
+			submitBtn.click();
+		}
+	});
+
+
+	submitBtn.addEventListener('click', function() {
+		const li = document.createElement("li");
+		let inputText = document.getElementById('inputText').value;
+		if (inputText.replace(/\s/g, '')) {
+			let t = document.createTextNode('<a href="#">' + inputText + '</a>');
+			li.appendChild(t);
+			ul.appendChild(li);
+			document.getElementById('inputText').value = '';
+		}
+	});
+}
+	
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
